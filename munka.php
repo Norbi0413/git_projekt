05162e50bd8fb4c43cef8fa60b9c0b468cdc710e
Norbi <e18.zachar.norbert@palfy-vizugyi.hu>
2023-03-30 12:41:34 +0200

    Itt is változás

--- munka.php
+++ munka.php
@@ -5,8 +5,8 @@
 //Összeadás
 //-------------------
 
-$a = 10;
-$b= 15;
+$a = 105;
+$b= 150;
 
 print $a + $b;
 
@@ -17,5 +17,7 @@ $x = 35;
 $y = 19;
 
 print '<br /><br />' . $x - $y;
+
+print "Konfliktus lesz...";
 
 ?>
